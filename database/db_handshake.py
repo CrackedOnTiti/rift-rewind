63a36679b58d1633d81324d325372e4a187a7ca4
hugopoggetti <hugo.poggetti@epitech.eu>
2025-10-16 15:07:04 +0200

    [Feat] update boto3 version to 10.40.53

--- database/db_handshake.py
+++ database/db_handshake.py
@@ -6,10 +6,18 @@ load_dotenv()
 
 def get_dynamodb_reasources():
     region = os.getenv('AWS_DEFAULT_REGION')
-    
+ 
     if not region:
         raise ValueError(
             "AWS_DEFAULT_REGION not found in environment variables! "
             "Make sure .env file exists and contains the correct reigon"
         )
     return boto3.resource('dynamodb', region_name=region)
+
+s3 = boto3.resource('s3')
+
+for bucket in s3.buckets.all():
+    print(bucket.name)
+
+# s3.upload_file('local.txt', 'mon-bucket', 'dossier/remote.txt')
+# s3.download_file('mon-bucket', 'dossier/remote.txt', 'local_copy.txt')
