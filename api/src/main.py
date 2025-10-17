8d4c9ed8090c648d78d8e30934205aff712fbe53
hugopoggetti <hugo.poggetti@epitech.eu>
2025-10-17 10:12:33 +0200

    [Add] app/index.html for route /

--- api/src/main.py
+++ api/src/main.py
@@ -1,11 +1,14 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, send_from_directory
 from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
+
+FRONTEND_FOLDER = '/app/frontend'
 
 @app.route('/')
 def index():
-    return 'Hello World'
+    return send_from_directory(FRONTEND_FOLDER, 'index.html')
 
 if __name__ == '__main__':
     app.run()
