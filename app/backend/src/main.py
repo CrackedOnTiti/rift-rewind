bc972db6948a85c96259656919cc6fecdeceeaf6
hugopoggetti <hugo.poggetti@epitech.eu>
2025-10-31 14:59:27 +0100

    [Feat] display map @/map

--- app/backend/src/main.py
+++ app/backend/src/main.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify, request, send_from_directory
 from flask_cors import CORS
+import os
 
 app = Flask(__name__)
 CORS(app)
@@ -18,26 +19,13 @@ def map_view():
 def public_files(filename):
     return send_from_directory(PUBLIC_FOLDER, filename)
 
-@app.route("/models/<path:filename>")
+@app.route("/assets/<path:filename>")
 def serve_models(filename):
-    models_dir = os.path.join(os.getcwd(), "frontend", "models")
+    models_dir = os.path.join(os.getcwd(), "frontend", "assets")
     file_path = os.path.join(models_dir, filename)
-    
-    if not os.path.exists(file_path):
-        os._exit(9)
-    
     directory = os.path.dirname(file_path)
     file_name = os.path.basename(file_path)
-    
     return send_from_directory(directory, file_name)
-    print(f"app/frontend/models {filename}")
-    return send_from_directory("app/frontend/models", filename)
-
-import os
-NODE_MODULES = os.path.join(os.getcwd(), "frontend", "node_modules")
-@app.route('/node_modules/<path:path>')
-def send_node_modules(path):
-    return send_from_directory(NODE_MODULES, path)
 
 @app.route('/api/rewind', methods=['POST'])
 def rewind():
